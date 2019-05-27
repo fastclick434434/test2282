51bc34cef4c34f79a4a13a5931da4e24628a0740
fastclick434434 <50154974+fastclick434434@users.noreply.github.com>
2019-05-27 23:36:37 +0300

    Add files via upload

---
+++ precache-manifest.4d3c98ab8f1ce8eb6203b3a04a6b7f8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8c7523284f2d0c11e9ff420992eb274",
+    "url": "/test2282/static/media/template.e8c75232.png"
+  },
+  {
+    "revision": "3c377f096cc8218381c9",
+    "url": "/test2282/static/js/runtime~main.776fc983.js"
+  },
+  {
+    "revision": "44523cdcfb50c2422e03",
+    "url": "/test2282/static/js/main.aabbaaaf.chunk.js"
+  },
+  {
+    "revision": "a03224992ac56bfac76d",
+    "url": "/test2282/static/js/2.00193922.chunk.js"
+  },
+  {
+    "revision": "a03224992ac56bfac76d",
+    "url": "/test2282/static/css/2.2836aca2.chunk.css"
+  },
+  {
+    "revision": "4dfa875670bd1fe27638b731221c2915",
+    "url": "/test2282/index.html"
+  }
+];
